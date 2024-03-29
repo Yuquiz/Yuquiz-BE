2f342fcc32a5d13687ad097767b6b4aec5f06b58
Allen <valternatean13@gmail.com>
2024-03-29 10:20:19 +0700

    feat: complete routes

---
+++ routes/quiz.route.js
@@ -0,0 +1,11 @@
+import express from "express";
+import controller from "../controllers/quiz.controller.js";
+const ROUTER = express.Router();
+
+ROUTER.get("/", controller.index); // Get all
+ROUTER.get("/:id", controller.getOne); // Get with id
+ROUTER.post("/", controller.store); // Create something
+ROUTER.put("/:id", controller.edit); // Update with id
+ROUTER.delete("/:id", controller.destroy); // Delete with id
+
+export default ROUTER;
---
+++ routes/router.js
@@ -0,0 +1,16 @@
+import express from "express";
+import user from "./user.route.js";
+import quiz from "./quiz.route.js";
+import question from "./question.route.js";
+import questionChoice from "./questionChoice.route.js";
+import answer from "./answer.route.js";
+
+const ROUTER = express.Router();
+ROUTER.use("/user", user);
+ROUTER.use("/quiz", quiz);
+ROUTER.use("/question", question);
+ROUTER.use("/qchoice", questionChoice);
+ROUTER.use("/qanswer", answer);
+
+export default ROUTER;
+
