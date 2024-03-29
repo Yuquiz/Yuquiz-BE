23d92c68df7e9cc8f04f939ae8e467fd30d66328
Allen <valternatean13@gmail.com>
2024-03-29 09:46:35 +0700

    feat: add db connection

---
+++ models/db.js
@@ -0,0 +1,24 @@
+import mysql from "mysql";
+import dotenv from "dotenv";
+
+dotenv.config();
+const connection = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: "quizin"
+})
+
+const query = (sql, data) => {
+    if(data.length != 0) {
+        sql = mysql.format(sql, data);
+    }
+
+    connection.query(sql, (err, res) => {
+        if(err) {
+            console.log(err.msg);
+        }
+    });
+}
+
+export default query;
