2af47aec85f8015ed1b91a2702fe06cc5e852a96
Allen <valternatean13@gmail.com>
2024-03-29 10:23:38 +0700

    fix: mismatched db name with env

--- models/db.js
+++ models/db.js
@@ -6,7 +6,7 @@ const connection = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: "quizin"
+    database: process.env.DB_NAME
 })
 
 const query = (sql, data) => {
