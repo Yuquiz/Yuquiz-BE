0b14ef95c9697de92efd960c2ad642ab967ebb06
Allen <valternatean13@gmail.com>
2024-03-30 10:19:40 +0700

    feat: add CRUD for `quiz` endpoint

--- controllers/quiz.controller.js
+++ controllers/quiz.controller.js
@@ -1,21 +1,78 @@
+import model from "../models/quiz.model.js"
+
 export default {
     index: function(req, res) {
-        res.send("Get all");
+        model.getAll(res);
     },
 
     getOne: function(req, res) {
-        res.send(`Get ${req.params.id}`);
+        if(isNaN(req.params.id)) {
+            res.status(400);
+            res.send({ msg: "id should be a number" })
+            return;
+        }
+
+        model.getById(req.params.id, res);
     },
 
     store: function(req, res) {
-        res.send(`Store`);
+        const dataComplete = (
+            req.body["user_ID"] != undefined
+            && req.body["name"] != undefined
+            && req.body["duration"] != undefined
+        )
+
+        const dataTypeCorrect = (
+            !isNaN(req.body["user_ID"])
+            && !isNaN(req.body["duration"])
+        )
+
+        if( !(dataComplete && dataTypeCorrect) ) {
+            const errorMsg = [];
+            if(!dataComplete) {
+                errorMsg.push("'user_ID', 'name', and 'duration' was not all provided");
+            } 
+
+            if(!dataTypeCorrect) {
+                errorMsg.push("'user_ID' and 'duration' should be a number");
+            }
+
+            res.status(400);
+            res.send({msg: errorMsg});
+            return;
+        }
+
+        const data = [
+            ["user_ID", "name", "duration"],
+            [req.body["user_ID"], req.body["name"], req.body["duration"]]
+        ]
+        model.store(data, res);
     },
 
     edit: function(req, res) {
-        res.send(`Edit ${req.params.id}`);
+        // Ignore when doesn't exist
+        const dataTypeCorrect = (req.body["duration"] == undefined 
+                            || !isNaN(req.body["duration"]));
+        if( !dataTypeCorrect ) {
+            const errorMsg = [];
+
+            if(!dataTypeCorrect) { errorMsg.push("'duration' should be a number"); }
+
+            res.status(400);
+            res.send({msg: errorMsg});
+            return;
+        }
+
+        model.edit(req.params.id, req.body, res);
     },
 
     destroy: function(req, res) {
-        res.send(`Delete ${req.params.id}`);
+        if(isNaN(req.params.id)) {
+            res.status(400);
+            res.send({ msg: "id should be a number" })
+            return;
+        }
+
+        model.destroy(req.params.id, res);
     }
 }
---
+++ models/quiz.model.js
@@ -0,0 +1,70 @@
+import db from "./db.js";
+
+export default {
+    getAll: function(res) {
+        db.query("SELECT * FROM Quizzes", [], (err, result) => {
+            if(err) {
+                res.send({ msg: `Something went wrong (${err.errno} - ${err.code})`, }); 
+                return;
+            }
+
+            res.send({
+                msg: "Quiz data fetch success",
+                data: result
+            });
+        });
+    },
+
+    getById: function(id, res) {
+        db.query("SELECT * FROM Quizzes WHERE id=?", [id], (err, result) => {
+            if(err) {
+                res.send({ msg: `Something went wrong (${err.errno} - ${err.code})`, }); 
+                return;
+            } 
+
+            res.send({
+                msg: "Quiz data fetch success",
+                data: result
+            });
+        });
+    },
+
+    store: function(data, res) {
+        db.query("INSERT INTO Quizzes(??) VALUES (?)", data, (err, result) => {
+            if(err) {
+                res.send({ msg: `Something went wrong (${err.errno} - ${err.code})`, }); 
+                return;
+            } 
+
+            res.send({
+                msg: "Quiz created",
+                id: result.insertId,
+            });
+        });
+    },
+
+    edit: function(id, newData, res) {
+        db.query("UPDATE Quizzes SET ? WHERE id=?", [newData, id], (err, result) => {
+            if(err) {
+                res.send({ msg: `Something went wrong (${err.errno} - ${err.code})`, }); 
+                return;
+            } 
+
+            res.send({ msg: `Quiz data with id: ${id} updated`});
+        });
+        
+    },
+
+    destroy: function(id, res) {
+        db.query("DELETE FROM Quizzes WHERE id=?", [id], (err, result) => {
+            if(err) {
+                res.send({ msg: `Something went wrong (${err.errno} - ${err.code})`, }); 
+                return;
+            } 
+
+            res.send({
+                msg: (result.affectedRows > 0?  `Deleted quiz with id:${id}`: "Nothing to delete"),
+            });
+        });
+    }
+}
