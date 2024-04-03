50ece656b875e2f864f477f467873b6604758971
Allen <valternatean13@gmail.com>
2024-04-03 22:52:16 +0700

    feat: add simple auth token checker

--- middlewares/authJWT.js
+++ middlewares/authJWT.js
@@ -1,4 +1,10 @@
 export default function authJWT(req, res, next) {
+    const token = req.headers.authorization?.replace("Bearer: ", "");
+    
+    if(token === undefined) {
+        return res.status(401).send({msg: "No token provided in request"});
+    }
+
     console.log("Your token: " + req.headers.authorization);
     next();
 }
