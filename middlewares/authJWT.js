ce2eedf5bc40cd6a0be6c055a76d3d6081fb984f
Allen <valternatean13@gmail.com>
2024-04-04 14:13:23 +0700

    feat: add token validator
    To get resource, an user should be logged in to the app

--- middlewares/authJWT.js
+++ middlewares/authJWT.js
@@ -1,10 +1,21 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
 export default function authJWT(req, res, next) {
-    const token = req.headers.authorization?.replace("Bearer: ", "");
+    const token = req.headers.authorization?.replace("Bearer ", "");
     
     if(token === undefined) {
         return res.status(401).send({msg: "No token provided in request"});
     }
 
-    console.log("Your token: " + req.headers.authorization);
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedData) => {
+        if(err) {
+            return res.status(500).send({msg: err})
+        }
+
+        console.log(decodedData);
+    })
+
     next();
 }
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,7 +1,9 @@
 import express from "express";
 import controller from "../controllers/user.controller.js"
+import authJWT from "../middlewares/authJWT.js";
 const ROUTER = express.Router();
 
+ROUTER.use(authJWT);
 ROUTER.get("/", controller.index); // Get all
 ROUTER.get("/:id", controller.getOne); // Get with id
 ROUTER.post("/", controller.store); // Create something
