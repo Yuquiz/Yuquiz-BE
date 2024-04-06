bee7a27a07145dca01213ef479c06e1ea3f1072a
Allen <valternatean13@gmail.com>
2024-04-06 21:23:56 +0700

    ref: separate error list

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,28 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-const ERRORS = {
-    "auth_reject": {
-        code: 401,
-        message: "Provided credentials is not valid"
-    },
-    "bad_request": {
-        code: 400,
-        message: "Your request contains something not accepted by server"
-    },
-    "not_found": {
-        code: 404,
-        message: "Requested resource doesn't exist in server"
-    },
-    "sql_error": {
-        code: 500,
-        message: "An error has occured during server database operation"
-    },
-    "token_missing_or_invalid": {
-        code: 403,
-        message: "You need a valid token to access this resource" 
-    },
-};
+import ERRORS from "./errorHandler.errors.js";
 
 export default function errorHandler(err, req, res, next) {
     const caughtError = ERRORS[err.code]
