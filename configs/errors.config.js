c4d3e6353784abe8218ee7f71c562c338854d28e
Allen <valternatean13@gmail.com>
2024-04-12 17:40:37 +0700

    style: remove extra new lines

--- configs/errors.config.js
+++ configs/errors.config.js
@@ -1,30 +1,38 @@
 export default {
-    "auth_reject": {
-        "code": 401,
-        "message": "Provided credentials is not valid"
+    "username_used": {
+        code: 401,
+        message: "Provided username is already used"
     },
-    "bad_request": {
-        "code": 400,
-        "message": "Your request contains something not accepted by server"
+    "invalid_credentials": {
+        code: 401,
+        message: "Provided credentials is not valid"
+    },
+    "insufficient_data": {
+        code: 400,
+        message: "Provided request doesn't have enough data to be used by server"
+    },
+    "illegal_request": {
+        code: 400,
+        message: "Provided request contains something not accepted by server"
     },
     "internal_error": {
-        "code": 500,
-        "message": "Unknown internal server error"
+        code: 500,
+        message: "Unknown internal server error"
     },
     "not_found": {
-        "code": 404,
-        "message": "Requested resource doesn't exist in server"
+        code: 404,
+        message: "Requested resource doesn't exist in server"
     },
     "query_error": {
-        "code": 500,
-        "message": "An error has occured during server database operation"
+        code: 500,
+        message: "An error has occured during server database operation"
     },
     "token_missing_or_invalid": {
-        "code": 403,
-        "message": "You need a valid token to access this resource" 
+        code: 403,
+        message: "A valid token needed to access this resource" 
     },
     "token_expired": {
-        "code": 403,
-        "message": "Your token has expired, consider logging in again to obtain a new one" 
+        code: 403,
+        message: "Token has expired, consider logging in again to obtain a new one" 
     },
 }
