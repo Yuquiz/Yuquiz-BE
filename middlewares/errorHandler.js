ce37b1854e111085defdfaebbb1f9c6adddb5c2e
Allen <valternatean13@gmail.com>
2024-04-25 08:09:02 +0700

    ref: remove unnecessary conditional

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -4,13 +4,7 @@ dotenv.config();
 import ERRORS from "../configs/errors.config.js";
 
 export default function errorHandler(err, req, res, next) {
-    if(err.code == undefined || err.code == "internal_error") { // for internal errors
-        return res.status(500).send({
-            message: ERRORS["internal_error"].message,
-            reason: (process.env.NODE_ENV == "DEVEL"? err: "[DISCLOSED]")
-        })
-    } 
-
+    err.code = err.code == undefined? "internal_error": err.code;
     const {code, message} = ERRORS[err.code];
     return res.status(code).send({
         message: message,
