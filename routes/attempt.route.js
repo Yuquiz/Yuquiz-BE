075eedc8d62e61853cff37fea7d3d584ccc4f4a2
Allen <valternatean13@gmail.com>
2024-04-25 09:35:24 +0700

    feat: apply role access middleware to endpoints

--- routes/attempt.route.js
+++ routes/attempt.route.js
@@ -1,13 +1,14 @@
 import express from "express";
 import controller from "../controllers/attempt.controller.js"
+import roleAccessWare from "../middlewares/roleAccess.js";
 const ROUTER = express.Router();
 
-ROUTER.get("/", controller.index); // Get all
-ROUTER.get("/quiz/:id", controller.user); // Get users who did the quiz with id
-ROUTER.get("/user/:id", controller.quiz); // Get quiz which was done by user with id
-ROUTER.get("/:id", controller.getOne); // Get with id
-ROUTER.post("/", controller.store); // Create something
-ROUTER.put("/:id", controller.edit); // Update with id
-ROUTER.delete("/:id", controller.destroy); // Delete with id
+ROUTER.get("/", roleAccessWare, controller.index); // Get all
+ROUTER.get("/quiz/:id", roleAccessWare, controller.user); // Get users who did the quiz with id
+ROUTER.get("/user/:id", roleAccessWare, controller.quiz); // Get quiz which was done by user with id
+ROUTER.get("/:id", roleAccessWare, controller.getOne); // Get with id
+ROUTER.post("/", roleAccessWare, controller.store); // Create something
+ROUTER.put("/:id", roleAccessWare, controller.edit); // Update with id
+ROUTER.delete("/:id", roleAccessWare, controller.destroy); // Delete with id
 
 export default ROUTER;
--- routes/roomQuiz.route.js
+++ routes/roomQuiz.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import controller from "../controllers/roomQuiz.controller.js"
+import roleAccessWare from "../middlewares/roleAccess.js";
 const ROUTER = express.Router();
 
-ROUTER.get("/", controller.index); // Get all
-ROUTER.get("/:id", controller.getOne); // Get with id
-ROUTER.post("/", controller.store); // Create something
-ROUTER.put("/:id", controller.edit); // Update with id
-ROUTER.delete("/:id", controller.destroy); // Delete with id
+ROUTER.get("/", roleAccessWare, controller.index); // Get all
+ROUTER.get("/:id", roleAccessWare, controller.getOne); // Get with id
+ROUTER.post("/", roleAccessWare, controller.store); // Create something
+ROUTER.put("/:id", roleAccessWare, controller.edit); // Update with id
+ROUTER.delete("/:id", roleAccessWare, controller.destroy); // Delete with id
 
 export default ROUTER;
