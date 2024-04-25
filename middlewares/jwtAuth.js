2c6ca50a12990e2dedf7d61c9b05dc4713e40ce6
Allen <valternatean13@gmail.com>
2024-04-25 08:16:56 +0700

    feat: apply errorHandler to `jwtAuth`

--- middlewares/jwtAuth.js
+++ middlewares/jwtAuth.js
@@ -5,15 +5,15 @@ dotenv.config();
 export default function authJWT(req, res, next) {
     const token = req.headers.authorization?.replace("Bearer ", "");
     if(token === undefined) {
-        return res.status(401).send({msg: "No token provided in request"});
+        return next({code: "token_missing_or_invalid", message: "No token provided"})
     }
 
     const err = jwt.verify(token, process.env.JWT_SECRET, (err, decodedData) => {
-        if(err) { return err }
+        if(err) return err
         req.id = decodedData.id;
         req.role = decodedData.role;
     })
-    if(err) { return res.status(403).send({msg: err}) }
+    if(err) return next({code: "token_missing_or_invalid", message: err})
 
     next();
 }
