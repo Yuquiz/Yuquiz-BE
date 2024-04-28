2c54170496a8af94e6fe8ed55233f0f78b044dcd
Allen <valternatean13@gmail.com>
2024-04-28 14:23:43 +0700

    feat: add `/app/dashboard` and `/app/profile`

---
+++ controllers/profile.controller.js
@@ -0,0 +1,12 @@
+import user from "../models/Users.js";
+
+export default {
+    index: async function(req, res, next) {
+        await user.getById(req.id)
+            .then(result => res.send({
+                    msg: `Hello, ${result["name"]} (U#${result["id"]})`,
+                    info: result,
+                }
+            )).catch(err => {errorOccured = true; next(err)})
+    }
+}
--- routes/app.route.js
+++ routes/app.route.js
@@ -1,9 +1,13 @@
 import express from "express";
 import leaderboard from "../controllers/leaderboard.controller.js"
+import dashboard from "../controllers/dashboard.controller.js"
+import profile from "../controllers/profile.controller.js"
+
+import jwtAuthWare from "../middlewares/jwtAuth.js";
 const ROUTER = express.Router();
 
 ROUTER.get("/leaderboard/:id", leaderboard.index); // id is quizId
-ROUTER.get("/dashboard/:id", leaderboard.index); // Get with id
-ROUTER.get("/profile/:id", leaderboard.index); // Get with id
+ROUTER.get("/dashboard", jwtAuthWare, dashboard.index);
+ROUTER.get("/profile", jwtAuthWare, profile.index);
 
 export default ROUTER;
