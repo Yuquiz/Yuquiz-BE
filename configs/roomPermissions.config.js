be8aa10e39af1c29d83d51e7de5dab02164d5bd9
Allen <valternatean13@gmail.com>
2024-04-28 19:41:40 +0700

    chore: add more permissions

--- configs/roomPermissions.config.js
+++ configs/roomPermissions.config.js
@@ -1,10 +1,12 @@
 export const PERMISSIONS = {
     "owner":  {
         "room": ["GET", "POST", "PUT", "DELETE"],
+        "roomPermission": ["GET", "POST", "PUT", "DELETE"],
         "roomQuiz": ["GET", "POST", "PUT", "DELETE"],
     },
     "participant":   {
         "room": ["GET", "POST", "PUT", "DELETE"],
+        "roomPermission": ["GET"],
         "roomQuiz": ["GET"],
     }
 
