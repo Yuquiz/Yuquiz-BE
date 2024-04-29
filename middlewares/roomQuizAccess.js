cf2ea11e393b597bf54030783283c0529b471758
Allen <valternatean13@gmail.com>
2024-04-29 07:34:17 +0700

    feat: protect `roomQuiz` routes
    Protected routes could only accessed if either:
    - user is superadmin or admin
    - user is given permission to the privateRoom which the quiz was inserted on

--- middlewares/roomQuizAccess.js
+++ middlewares/roomQuizAccess.js
@@ -1,3 +1,4 @@
+import roomQuiz from "../models/RoomQuizzes.js";
 import roomPermission from "../models/RoomPermissions.js";
 import room from "../models/PrivateRooms.js";
 import {PERMISSIONS} from "../configs/roomPermissions.config.js"
@@ -6,18 +7,24 @@ import {PERMISSIONS} from "../configs/roomPermissions.config.js"
 export default async function(req, res, next) {
     const ENDPOINT = "roomQuiz";
     let error = null;
-    let hasPermission = false;
+    let hasPermission = req.role != "user"; // Superadmin and admin bypasses ownership verification
 
-    await room.getById(req.params.id) // Is current user the owner of this room?
-        .then(result => hasPermission = req.id == result.user_id)    
-        .catch(err => error = err )
+    const roomId = await roomQuiz.getById(req.params.id)
+        .then(result => result?.room_id)
+        .catch(err => error = err)
+
+    if(!hasPermission) {
+        await room.getById(roomId) // Is current user the owner of this room?
+            .then(result => hasPermission = req.id == result?.user_id)    
+            .catch(err => error = err )
+    }
 
     if(!hasPermission) { 
-        await roomPermission.roomByUserId(req.id) // Does the current user has permission to this room?
+        await roomPermission.usersByRoomId(roomId) // Does current user has permission to this room?
             .then(result => {
                 let ptr = 0;
                 while(ptr < result.length && !hasPermission) {
-                    hasPermission = req.params.id == result[ptr].room_id;
+                    hasPermission = req.id == result[ptr].participant_id;
                     ptr++
                 }
             })
@@ -34,7 +41,7 @@ export default async function(req, res, next) {
     if(!hasPermission) {
         return next( {
             code: "no_private_access", 
-            message: "You don't have permission to this room!"
+            message: "You don't have permission to this room to access this roomQuiz!"
         })
     }
 
--- routes/roomQuiz.route.js
+++ routes/roomQuiz.route.js
@@ -5,9 +5,9 @@ import roomQuizAccessWare from "../middlewares/roomQuizAccess.js";
 const ROUTER = express.Router();
 
 ROUTER.get("/", roleAccessWare, controller.index); // Get all
-ROUTER.get("/:id", roleAccessWare, controller.getOne); // Get with id
+ROUTER.get("/:id", [roleAccessWare, roomQuizAccessWare], controller.getOne); // Get with id
 ROUTER.post("/", roleAccessWare, controller.store); // Create something
-ROUTER.put("/:id", roleAccessWare, controller.edit); // Update with id
-ROUTER.delete("/:id", roleAccessWare, controller.destroy); // Delete with id
+ROUTER.put("/:id", [roleAccessWare, roomQuizAccessWare], controller.edit); // Update with id
+ROUTER.delete("/:id", [roleAccessWare, roomQuizAccessWare], controller.destroy); // Delete with id
 
 export default ROUTER;
