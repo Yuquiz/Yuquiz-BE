0bb55eb2b4c512509e1857307954915b1fb26e37
Allen Rasheed <93359407+solsteace@users.noreply.github.com>
2024-04-29 10:49:43 +0700

    chore: update `connectTimeout` on db.js

--- configs/db.js
+++ configs/db.js
@@ -2,13 +2,13 @@ import mysql from "mysql";
 import dotenv from "dotenv";
 
 dotenv.config();
-const connection = mysql.createPool({
+const connection = mysql.createPool({ // https://github.com/mysqljs/mysql#connection-options
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
-    connectTimeout: 30000 // https://github.com/mysqljs/mysql#connection-options
+    connectTimeout: 10001 // https://stackoverflow.com/questions/35553432/error-handshake-inactivity-timeout-in-node-js-mysql-module
 })
 
 export default {
